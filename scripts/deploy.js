3368fb6a7dff1ae25af1361eb03ab2f392224a46
vijay ankith <vijay2ankith@gmail.com>
2022-06-06 15:00:52 +0530

    fixed svg image

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,16 +6,16 @@ const main = async () => {
     console.log("Contract deployed to:", domainContract.address);
 
     // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
-    let txn = await domainContract.register("banana", { value: hre.ethers.utils.parseEther('0.1') });
+    let txn = await domainContract.register("kryptokoders", { value: hre.ethers.utils.parseEther('0.1') });
     await txn.wait();
     console.log("Minted domain banana.mat");
 
-    txn = await domainContract.setRecord("banana", "Am I a banana or a mat??");
+    txn = await domainContract.setRecord("kryptokoders","");
     await txn.wait();
-    console.log("Set record for banana.mat");
+    console.log("Set record for kryptokoders.mat");
 
-    const address = await domainContract.getAddress("banana");
-    console.log("Owner of domain banana:", address);
+    const address = await domainContract.getAddress("kryptokoders");
+    console.log("Owner of domain kryptokoders:", address);
 
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
